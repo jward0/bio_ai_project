c48d8dea642176b0553b21f010d3bfc1222015b9
James Ward <jward11235@gmail.com>
2022-04-04 16:14:08 +0100

    Completed traffic and demand graph construction and visualisation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.sparse.csgraph import floyd_warshall
 
@@ -5,32 +6,61 @@ from scipy.sparse.csgraph import floyd_warshall
 class TrafficGraph:
 
     def __init__(self):
-        self.weights = np.empty(shape=(12, 25, 25))
-        self.desires = np.empty(shape=(12, 25, 25))
-
+        # 12 time steps, 25 (5x5 grid) vertices
         # We want our default values for weights to be infinity (no connection)
         # And for desires to be 0 (no demand)
-        self.weights[0] = np.ones(shape=(25, 25)) * np.inf
-        self.desires[0] = np.zeros(shape=(25, 25))
+        self.traffic = np.ones(shape=(12, 25, 25)) * np.inf
+        self.demand = np.zeros(shape=(12, 25, 25))
 
         # Initialise random weights and demands on edges between adjacent vertices
         for i in range(25):
             if i % 5 != 0:
-                self.weights[i][i-1] = np.randint(5, 16)
-                self.desires[i][i-1] = np.randint(5, 16)
+                self.traffic[0][i][i-1] = np.random.randint(1, 11)
+                self.demand[0][i][i-1] = np.random.randint(1, 11)
             if i % 5 != 4:
-                self.weights[i][i+1] = np.randint(5, 16)
-                self.desires[i][i+1] = np.randint(5, 16)
+                self.traffic[0][i][i+1] = np.random.randint(1, 11)
+                self.demand[0][i][i+1] = np.random.randint(1, 11)
             if i > 4:
-                self.weights[i][i-5] = np.randint(5, 16)
-                self.desires[i][i-5] = np.randint(5, 16)
+                self.traffic[0][i][i-5] = np.random.randint(1, 11)
+                self.demand[0][i][i-5] = np.random.randint(1, 11)
             if i < 20:
-                self.weights[i][i+5] = np.randint(5, 16)
-                self.desires[i][i+5] = np.randint(5, 16)
+                self.traffic[0][i][i+5] = np.random.randint(1, 11)
+                self.demand[0][i][i+5] = np.random.randint(1, 11)
 
+        # Populate all 12 time steps with random noise added to legal values, clipped to [0, 10]
         for i in range(11):
-            self.weights[i+1] = self.weights[i]
-            self.desires[i+1] = self.desires[i]
+            for j in range(25):
+                for k in range(25):
+                    if self.traffic[i][j][k] != np.inf:
+                        self.traffic[i+1][j][k] = np.clip(self.traffic[i][j][k] + np.random.randint(-2, 3), 1, 10)
+                        self.demand[i+1][j][k] = np.clip(self.demand[i][j][k] + np.random.randint(-2, 3), 1, 10)
+
+    def visualise_traffic(self):
+        coords = [(i % 5, int(i/5)) for i in range(25)]
+
+        fig, axs = plt.subplots(3, 4)
+
+        for i in range(12):
+            for j in range(25):
+                for k in range(25):
+                    if self.traffic[i][j][k] != np.inf:
+                        axs[int(i/4), i % 4].plot([coords[j][0], coords[k][0]], [coords[j][1], coords[k][1]],
+                                                  'r', linewidth=self.traffic[i][j][k])
+
+        plt.show()
+
+    def visualise_demand(self):
+        coords = [(i % 5, int(i/5)) for i in range(25)]
+
+        fig, axs = plt.subplots(3, 4)
+
+        for i in range(12):
+            for j in range(25):
+                for k in range(25):
+                    axs[int(i/4), i % 4].plot([coords[j][0], coords[k][0]], [coords[j][1], coords[k][1]],
+                                              'g', linewidth=self.demand[i][j][k])
+
+        plt.show()
 
 
 class BusRoutes:
@@ -54,6 +84,8 @@ def calculate_cost(traffic_graph, bus_routes):
 
 def main():
     traffic_graph = TrafficGraph()
+    traffic_graph.visualise_traffic()
+    traffic_graph.visualise_demand()
     population = [BusRoutes() for _ in range(100)]
 
 
